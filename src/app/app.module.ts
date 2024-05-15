3507b7b8d33f9e488b80d4227cb708552930475a
TornikeJ <49530128+tornikej@users.noreply.github.com>
2024-05-15 17:52:49 +0400

    feat. lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientsModule } from './clients/clients.module';
 import { NgOptimizedImage } from '@angular/common';
-import { HomeModule } from './home/home.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { TransactionsModule } from './transactions/transactions.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -18,9 +15,6 @@ import { EffectsModule } from '@ngrx/effects';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ClientsModule,
-    TransactionsModule,
-    HomeModule,
     NgOptimizedImage,
     BrowserAnimationsModule,
     MatProgressBarModule,
